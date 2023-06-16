c44ef8352e6740a5b376499b90fe19cf95e76958
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-16 12:42:37 -0700

    if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+
+int main(void)
+{
+  if(n >= 1)
+{
+  printf("%d is positive", n);
+} else if(n <= 1)
+{
+  printf("%d is negative", n);
+} else {
+    print("%d is 0", n);
+}
+  return 0;
+}
