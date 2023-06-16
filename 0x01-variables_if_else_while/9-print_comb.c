ed98c14426cfe4dd222a7a0a88a21914e600f1ab
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-16 15:36:13 -0700

    separate

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base
+* 10 starting from 0, separated by a comma and space. 
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int b = 0;
+while (b < 10)
+{
+putchar(48 + b);
+if (b != 9)
+{
+putchar(44);
+putchar(32);
+b++;
+}
+else
+{
+b++;
+}
+}
+putchar('\n');
+return (0);
+}
