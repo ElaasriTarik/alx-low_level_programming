de4a2dfb9b6f379ff2cd9f9e4b56759074a53a72
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-16 15:37:12 -0700

    separated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - prints all single digit numbers of base
-* 10 starting from 0, separated by a comma and space. 
+* 10 starting from 0, separated by a comma and space.
 * followed by a new line
 * Return: Always 0 (Success)
 */
