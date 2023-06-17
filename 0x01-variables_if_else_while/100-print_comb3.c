828c60d20758b82c1881006d09d2633fa2cc672a
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-16 17:07:14 -0700

    two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Returns: 0 Always
+ */
+int main(void)
+{
+int c, d = 0;
+for (;d < 10; d++)
+{
+c = 0;
+for (;c < 10; c++)
+{
+if (d != c && d < c)
+{
+putchar('0' + d);
+putchar('0' + c);
+
+if (c + d != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
