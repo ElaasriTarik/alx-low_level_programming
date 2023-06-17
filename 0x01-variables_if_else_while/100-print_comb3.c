6ca30e71ec24c71a078bd5957042763edf76b68a
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-16 17:08:52 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Returns: 0 Always
+ * Return: 0 Always
  */
 int main(void)
 {
 int c, d = 0;
-for (;d < 10; d++)
+for (; d < 10; d++)
 {
 c = 0;
-for (;c < 10; c++)
+for (; c < 10; c++)
 {
 if (d != c && d < c)
 {
