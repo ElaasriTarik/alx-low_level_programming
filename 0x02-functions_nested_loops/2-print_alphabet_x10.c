21c1b95794693354b3d671d3df877174511bd3d4
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-20 15:30:01 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets x10 fllowd by a new lines
+ * Void: No retuen
+ */
+void print_alphabet_x10(void)
+{
+char a = 'a';
+for (; a <= 'j'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
