895d0f40064048d5a9ad9328ffc954b44581115d
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-20 16:09:52 -0700

    g

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number, fllowd by a new lines
+ * Void: No retuen
+ */
+int print_sign(int r)
+{
+if (r > 0)
+{
+printf("+");
+return (1);
+}
+else if (r < 0)
+{
+printf("0");
+return (0);
+}
+else
+{
+printf("-");
+return (0);
+}
+}
