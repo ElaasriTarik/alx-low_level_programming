43a443b351db12793af935e5892c686b4488b987
ElaasriTarik <elaasriya2003@gmail.com>
2023-06-20 15:49:18 -0700

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _islower - return 1 if true, 0 if false, fllowd by a new lines
- * Void: No retuen
+ *@a letter as test
+ * _islower return 0 or 1
  */
 int _islower(int a)
 {
-  if (a >= 97 && a <= 122)
-    {
-      return 1;
-    }
-  return 0;
+if (a >= 97 && a <= 122)
+{
+  return (1);
+}
+ return (0);
 }
