bd918c41265d7887f3f7d7428edba3350d5fa1df
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-21 02:20:20 +0100

    103 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints even valued term numbers, starting with 1 and 2,
+ * Return: Always (0).
+ */
+int main()
+{
+int i=1,j=1,sum=0;
+while(i<4000000)
+{
+i=i+j;
+j=i-j;
+if(i%2==0)
+sum+=i;
+}
+printf("Sum is: %d",sum);
+}
