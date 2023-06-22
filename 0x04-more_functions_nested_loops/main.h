f487398627046afc919c7f2ce86e74624b0e16b9
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 14:07:38 +0100

    uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+extern int _putchar(char);
+extern int _isupper(int);
+extern int _isdigit(int);
+extern int mul(int a, int b);
+extern void print_numbers(void);
+extern void print_most_numbers(void);
+extern void more_numbers(void);
+extern void print_line(int);
+extern void print_diagonal(int);
+extern void print_square(int size);
+extern void print_triangle(int size);
+extern void print_number(int);
+#endif
