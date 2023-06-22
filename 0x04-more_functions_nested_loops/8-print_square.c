9bd1291f1fbc1cb0b51d7c5e7506c839b72f6698
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 23:00:17 +0100

    aa

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - Print a square.
+ * @size : The number of '#' characters to use
+ * Return: Void.
+ */
+void print_square(int size)
+{
+int x;
+int sp;
+
+for (x = 0; x < size; x++)
+{
+sp = 0;
+while (sp < 10)
+{
+_putchar('#');
+sp++;
+}
+_putchar('\n');
+}
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
