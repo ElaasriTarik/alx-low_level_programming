5420e11006e1e09636a97919804278b87b982eb1
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-23 00:49:47 +0100

    print a number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Print a number int .
+ * @n : The number of '\' characters to use
+ * Return: Void.
+ */
+void print_number(int n)
+{
+_putchar(n + '0');
+}
