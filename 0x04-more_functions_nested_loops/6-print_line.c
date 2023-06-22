33ec02ef1642b06c6fea34374a1b596ed6b0b9a3
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 15:33:07 +0100

    n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - Print _ n times.
+ * @n: number of times
+ * Return: Void.
+ */
+void print_line(int n)
+{
+if (n == 0)
+{
+_putchar('\n');
+}
+else
+{
+for (; n > 0; n--)
+{
+putchar('_');
+}
+}
+_putchar('\n');
+}
