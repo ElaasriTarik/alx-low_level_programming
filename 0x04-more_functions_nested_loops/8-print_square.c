d6a872445eeff478fd558b61f729367d29cec54c
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 23:11:18 +0100

    ff

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,7 @@ void print_square(int size)
 {
 int x;
 int sp;
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
+
 for (x = 0; x < size; x++)
 {
 sp = 0;
@@ -26,6 +21,8 @@ sp++;
 }
 _putchar('\n');
 }
+if (size <= 0)
+{
 _putchar('\n');
 }
 }
