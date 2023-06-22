762ed3002f7b80807bfe6edb4b7654673dacc7cd
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 14:11:31 +0100

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check if d is digit.
+ * @d: a character
+ * Return: Always 0.
+ */
+int _isupper(int d)
+{
+if (d >= 48 && d <= 57)
+return (1);
+else
+return (0);
+}
