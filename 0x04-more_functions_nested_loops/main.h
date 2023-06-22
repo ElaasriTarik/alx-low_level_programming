31d4fd694440d4faad51f39251dccda884207193
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 22:49:01 +0100

    kk

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,7 +8,7 @@ extern void print_numbers(void);
 extern void print_most_numbers(void);
 extern void more_numbers(void);
 extern void print_line(int);
-extern void print_diagonal(int);
+extern void print_diagonal(int n);
 extern void print_square(int size);
 extern void print_triangle(int size);
 extern void print_number(int);
