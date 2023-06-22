24f890877533b49792bb5cab3beed79b6f870271
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-22 23:30:44 +0100

    ee

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 int x;
 for (x = 1; x <= 100; x++)
 {
-if (x % 15 ==  0)
+if (x % 15 == 0)
 {
 printf("%s", "FizzBuzz");
 }
@@ -29,5 +29,6 @@ printf("%d", x);
 if (x != 100)
 {
 printf(" ");
+}
 }
 }
