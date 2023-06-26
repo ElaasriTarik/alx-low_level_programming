5dc8b7799ae2f22b1abebdea49ca2625d742d80d
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-27 00:05:07 +0100

    ff

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,7 @@
  * @src: the variable to be copied
  * @dest: the variable to copy into
  * Return value: the pointer to dest
+ * Return: dest (Success)
 */
 char *_strcpy(char *dest, char *src)
 {
@@ -13,7 +14,8 @@ do
 {
 dest[x] = src[x];
 x++;
-} while (src[x] != '\0');
+}
+while (src[x] != '\0');
 dest[x] = '\0';
 
 return (dest);
