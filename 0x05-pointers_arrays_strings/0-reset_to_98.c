518e223b6a486e0da0b317719c5aa499b477d21f
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-26 17:53:52 +0100

    change n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98: replace the value of an int to 98
+ * reset_to_98: replace the value of a pointer int *n to 98
  * @n: the int pointer
- * Return: always n;
+ * Return: n;
 */
 void reset_to_98(int *n)
 {
