c8dcce09566a413b742c3eab1391313e49cb2d89
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-27 01:59:12 +0100

    get numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 {
 int al = 1;
 int x = 0;
-unsigned int sun = 0;
+unsigned int sum = 0;
 /*check the sign of the number...*/
 while (s[x] != '\0' && !(s[x] <= 57 && s[x] >= 48))
 {
@@ -19,9 +19,9 @@ x++;
 }
 while (s[x] != '\0' && !(s[x] <= 57 && s[x] >= 48))
 {
-sun = (sun * 10) + (s[x] - '0');
+sum = (sum * 10) + (s[x] - '0');
 x++;
 }
-sun =sum * al;
-return (sun);
+sum = sum * al;
+return (sum);
 }
