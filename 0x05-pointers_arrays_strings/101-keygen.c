d94aa0c3f17f48308969369968c89a5b14c29c85
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-27 01:34:16 +0100

    f

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+/**
+ * main - generates a password
+ * Return: always 0 (Success)
+*/
 int main () {
-   int i, n;
-   time_t t;
-   
-   n = 5;
-   
-   /* Intializes random number generator */
-   srand((unsigned) time(&t));
+int i, n;
+char pass = "Tada! Congrats";
+time_t t;
+n = 5;
+/* Intializes random number generator */
+srand((unsigned) time(&t));
+while (pass[i] != '\0')
+{
+_putchar(pass[i])
+i++;
+}
 
-   /* Print 5 random numbers from 0 to 50 */
-   for( i = 0 ; i < n ; i++ ) {
-      printf("%d\n", rand() % 50);
-   }
-   
-   return(0);
+return(0);
 }
