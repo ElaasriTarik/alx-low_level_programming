d7349c83fbc788068f28366931fbfe3f17acbab2
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-06-29 17:40:39 +0100

    strncpy

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -3,13 +3,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i = 0;
-  while (i < n)
-  {
-    dest[i] = src[i];
-    i++;
-  }
-  return (dest);
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
 
 int main(void)
