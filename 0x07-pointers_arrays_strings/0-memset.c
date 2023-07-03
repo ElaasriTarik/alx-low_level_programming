08ebdab772ac0a7c0b4638159920df7c55841f6a
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-03 13:51:15 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * _memset - fill the n spots of memory with b
+ * @b: the value to be fillied with
+ * @n: the number of times to fill with 
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int x = 0;
+while (x < n)
+{
+s[x] = b;
+x++;
+}
+return (s);
+}
