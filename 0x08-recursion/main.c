25388e23c44ee9e2c2d81b1bd43e9a5393aa7864
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-05 15:30:47 +0100

    pow

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -11,18 +11,12 @@ int _pow_recursion(int x, int y)
 if (y <= 0)
 return (y);
 if (y > 1)
-{
 return (x * _pow_recursion(x, y - 1));
+else if (y == 0)
+return (0);
 }
 
-}
 
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
     int n;
