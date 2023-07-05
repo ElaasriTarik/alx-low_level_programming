7d971e9efb3b78d3561bccb405da872e4640f8ac
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-05 17:25:26 +0100

    pow

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *is_prime_number - return 1 is prime, 0 if not
+ *@n: number to check
+ *Return: 0 (Success)
+*/
+
+int is_prime_number(int n)
+{
+return cc(3, n);
+}
+
+/**
+ *cc - returns a prime number
+ *@n: number to check
+ *@d: int
+ *Return: n
+*/
+
+int cc(int d, int n)
+{
+if (n == 0 || n == 1)
+return (0);
+else if (x > n /2)
+else
+return (cc(d + 1, n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _get_sqr(int n, int x);
+int is_prime_number(int n);
+int cc(int d, int n);
 
 #endif
