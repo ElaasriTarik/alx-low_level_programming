933ff566aa5a784c44647fc6e015b502caf24c5a
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-07 13:50:48 -0700

    all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints name of program
+*@argc: argument count
+*@argv: arguments
+*Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+for(; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
