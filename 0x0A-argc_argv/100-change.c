9f24aa6792b704e9bf488ab9718af4579f701cb2
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-07 15:27:47 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,42 +10,39 @@
  */
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+int cs, tot = 0;
+
 if (argc != 2)
 {
 printf("Error\n");
-return (0);
+return (1);
 }
-cents = atoi(argv[1]);
-if (cents < 0)
+cs = atoi(argv[1]);
+while (cs > 0)
 {
-printf("0\n");
-}
-while (cents > 0)
+tot++;
+if ((cs - 25) >= 0)
 {
-coins++;
-if ((cents - 25) >= 0)
-{
-cents -= 25;
+cs -= 25;
 continue;
 }
-if ((cents - 10) >= 0)
+if ((cs - 10) >= 0)
 {
-cents -= 10;
+cs -= 10;
 continue;
 }
-if ((cents - 5) >= 0)
+if ((cs - 5) >= 0)
 {
-cents -= 5;
+cs -= 5;
 continue;
 }
-if ((cents - 2) >= 0)
+if ((cs - 2) >= 0)
 {
-cents -= 2;
+cs -= 2;
 continue;
 }
-cents--;
+cs--;
 }
-printf("%d", coins);
+printf("%d\n", tot);
 return (0);
 }
