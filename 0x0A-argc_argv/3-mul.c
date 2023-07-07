aa51e39d64b98326bfefe97f51282800480ddc0c
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-07 14:37:15 -0700

    ff

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int n1, n2;
+int n1, n2, res;
 
 if (argc != 3)
 {
@@ -20,6 +20,7 @@ return (1);
 
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+res = n1 *n2;
+printf("%d\n", res);
 return (0);
 }
