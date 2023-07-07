2e944a9344f5291c8553f61f0093e14b19117dc0
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-07 14:28:32 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
 *Return: Always 0.
 */
 
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 int n1, n2;
 
@@ -19,7 +19,7 @@ return (1);
 }
 
 n1 = atoi(argv[1]);
-n2 = atoi(rgv[2]);
+n2 = atoi(argv[2]);
 printf("%d\n", n1 * n2);
 return (0);
 }
