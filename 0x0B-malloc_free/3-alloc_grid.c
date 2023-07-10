f0fd484d6b58ae716999d9e165d2a3d405bda7f7
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-11 00:05:47 +0100

    arr malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @wisth: width of array
- * @s2: height of array
+ * @width: width of array
+ * @height: height of array
  * Return: (return array pointer after editing).
  */
 int **alloc_grid(int width, int height)
@@ -13,19 +13,21 @@ int i, j;
 int **ar;
 if (width <= 0 || height <= 0)
 return (NULL);
-ar = (int **)malloc(height * sizeof(int *));
+ar = (int **)malloc(sizeof(int *) * height);
 for (i = 0; i < height; i++)
 {
-ar[i] = (int *)malloc(width * sizeof(int *));
+ar[i] = (int *)malloc(sizeof(int *) * width);
 if (ar[i] == NULL)
 {
 free(ar);
-return NULL;
+for (j = 0; j <= i; j++)
+free(ar[j]);
+return (NULL);
 }
 }
 for (i = 0; i < height; i++)
 {
-for ( j = 0; j < width; j++)
+for (j = 0; j < width; j++)
 {
 ar[i][j] = 0;
 }
