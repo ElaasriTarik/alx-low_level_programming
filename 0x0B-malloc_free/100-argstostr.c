c7eeb0e087ffd21f592b868556dd00f63e8c716b
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-11 17:46:10 +0100

    concat an amount of strs

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,12 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * str_concat - concatenate two strings
- * @s1: first string
- * @s2: second string
- * Return: (return new piinter string).
+ * argstostr - concatenates n argument of stringd
+ * @ac: number of strings
+ * @av: array of strings
+ * Return: (Return a pointer).
  */
 char *argstostr(int ac, char **av)
 {
+int i , k, cc = 0;
+char *ptr;
+int count = 0;
+for (i = 1; i < ac; i++)
+{
+if (av[i] == NULL)
+{
+return (NULL);
+}
+k = 0;
+for (; av[i][k] != '\0'; k++)
+{
+count++;
+}
+}
+printf("%d \n", count);
+ptr = malloc(count * sizeof(char) + 1);
+if (ptr == NULL)
+return (NULL);
+i = 1;
+while (i < ac)
+{
+k = 0;
+while (av[i][k] != '\0')
+{
+ptr[cc] = av[i][k];
+cc++;
+k++;
+}
+ptr[cc] = '\n';
+cc++;
+i++;
+}
+ptr[cc] = '\n';
 
+return (ptr);
 }
