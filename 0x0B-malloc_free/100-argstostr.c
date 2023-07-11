3462ab9aca31d51b086611e786eccc65520e8f9a
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-11 17:43:12 +0100

    concat an amount of strs

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: (return new piinter string).
+ */
+char *argstostr(int ac, char **av)
+{
+
+}
--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -2,84 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void free_grid(int **grid, int height)
+char *argstostr(int ac, char **av)
 {
-int i;
-free(grid);
-for (i = 0; i < height; i++)
-{
-free(grid[i]);
-}
 
-}
-int **alloc_grid(int width, int height)
-{
-int i, j;
-int **ar;
-if (width <= 0 || height <= 0)
-return (NULL);
-ar = (int **)malloc(sizeof(int *) * height);
-if (ar == NULL)
-return (NULL);
-for (i = 0; i < height; i++)
-{
-ar[i] = (int *)malloc(sizeof(int) * width);
-if (ar[i] == NULL)
-{
-free(ar);
-for (j = 0; j <= i; j++)
-free(ar[j]);
-return (NULL);
-}
-}
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
-ar[i][j] = 0;
-}
-}
-return (ar);
-}
-
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
-
-    h = 0;
-    while (h < height)
+    int i , k, cc = 0;
+    char *ptr;
+    int count = 0;
+    for (i = 1; i < ac; i++)
     {
-        w = 0;
-        while (w < width)
+        if (av[i] == NULL)
         {
-            printf("%d ", grid[h][w]);
-            w++;
+            return (NULL);
         }
-        printf("\n");
-        h++;
-    }   
+         k = 0;
+        for (; av[i][k] != '\0'; k++)
+        {
+         count++;
+        }
+    }
+printf("%d \n", count);
+ptr = malloc(count * sizeof(char) + 1);
+if (ptr == NULL)
+return (NULL);
+i = 1;
+while (i < ac)
+    {
+        k = 0;
+        while (av[i][k] != '\0')
+        {
+        ptr[cc] = av[i][k];
+        cc++;
+        k++;
+        }
+        ptr[cc] = '\n';
+        cc++;
+        i++;
+    }
+    ptr[cc] = '\n';
+return (ptr);
+
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int **grid;
 
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
+int main(int ac, char *av[])
+{
+    char *s;
+
+    s = argstostr(ac, av);
+    if (s == NULL)
     {
         return (1);
     }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    free_grid(grid, 4);
+    printf("%s", s);
+    free(s);
     return (0);
 }
