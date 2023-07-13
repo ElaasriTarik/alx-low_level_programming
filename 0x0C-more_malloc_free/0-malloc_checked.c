35a69b602f5a30fd83dcd91435375b24e384e967
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-13 15:32:24 +0100

    malloc void

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,21 +3,19 @@
 #include <limits.h>
 
 /**
- * malloc_checked -  function that allocates memory using malloc
+ * malloc_checked - function that allocates memory using malloc
  *@b: size of memory to be allocated
  *
- * Return: (SUCCESS)
+ * Return: (pointer)
 */
 
 void *malloc_checked(unsigned int b)
 {
-void* ptr;
+void *ptr;
 ptr = malloc(b);
 if (ptr == NULL)
 {
 exit(98);
 }
-
 return (ptr);
-
 }
