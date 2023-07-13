4e8c996df0de3c35f1db13cfa7a50d0090a1511d
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-13 23:47:42 +0100

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *big_multiply(char *s1, char *s2);
 int len(char *str);
-int check_digit(int c)
+int check_digit(int c);
+
 #endif
