cda4f4e2a9e21cd3ca0743fbe1a64f26bcfa8248
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-13 17:11:10 +0100

    array _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - return an array of certain amount pointers
+ *@nmemb: element of the array
+ *@size: size of each element
+ * Return: (success)
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+if (nmemb == 0 || size == 0)
+return (NULL);
+void *pp;
+
+pp = (void *)malloc(nmemb * size);
+if (pp == NULL)
+return (NULL);
+
+return (pp);
+
+}
