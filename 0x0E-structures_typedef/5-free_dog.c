2199c8ce7925c78c52d66198a655edde99ec4769
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-17 21:19:42 +0100

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ * @d: struct dog variable
+ *
+ * Return: Nothing (success)
+ */
+
+void free_dog(dog_t *d)
+{
+free(d);
+}
