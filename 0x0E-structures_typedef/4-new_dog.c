6705d4f8ace1da684a6c70c1f5ba203f6c417a9c
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-17 21:15:49 +0100

    dog_t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
 int len(char *c);
-char *_cpy(char *des, char *src);
+char *char_copy(char *des, char *src);
 
 /**
- * new_dog - creates a copy of name and owner
+ * new_dog - creates a new dog atruct
  * @name: name of dog
  * @age: age
  * @owner: owner
+ *
  * Return: new struct
  */
 
@@ -19,33 +19,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 int namelen;
 int ownerlen;
 dog_t *dd;
-
 if (name != NULL && owner != NULL)
 {
 namelen = len(name) + 1;
 ownerlen = len(owner) + 1;
 dd = malloc(sizeof(dog_t));
-
 if (dd == NULL)
 return (NULL);
-
 dd->name = malloc(sizeof(char) *namelen);
-if (dd->name)
+if (dd->name == NULL)
 {
 free(dd->name);
 return (NULL);
 }
-
 dd->owner = malloc(sizeof(char) *ownerlen);
-if (dd->owner)
+if (dd->owner == NULL)
 {
 free(dd->name);
 free(dd);
 return (NULL);
 }
-
-dd->name = _cpy(dd->name, name);
-dd->owner = _cpy(dd->owner, owner);
+dd->name = char_copy(dd->name, name);
+dd->owner = char_copy(dd->owner, owner);
 dd->age = age;
 }
 return (dd);
@@ -65,12 +60,12 @@ c++;
 return (x);
 }
 /**
- * _cpy - to copy a string
+ * char_copy - to copy a string
  * @dest: Destination value
  * @src: Source value
  * Return: the pointer to dest
  */
-char *_cpy(char *des, char *src)
+char *char_copy(char *des, char *src)
 {
 int x = 0;
 while (src[x] != '\0')
@@ -79,6 +74,5 @@ des[x] = src[x];
 x++;
 }
 des[x] = '\0';
-printf("%s", des);
 return (des);
-} 
+}
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -3,7 +3,7 @@
 #include "dog.h"
 
 int len(char *c);
-char *_cpy(char *des, char *src);
+char *char_copy(char *des, char *src);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -20,23 +20,23 @@ dd = malloc(sizeof(dog_t));
 if (dd == NULL)
 return (NULL);
 
-dd->name = malloc(sizeof(char) *namelen);
-if (dd->name)
+dd->name = malloc(sizeof(char) * namelen);
+if (dd->name == NULL)
 {
 free(dd->name);
 return (NULL);
 }
 
-dd->owner = malloc(sizeof(char) *ownerlen);
-if (dd->owner)
+dd->owner = malloc(sizeof(char) * ownerlen);
+if (dd->owner == NULL)
 {
 free(dd->name);
 free(dd);
 return (NULL);
 }
 
-dd->name = _cpy(dd->name, name);
-dd->owner = _cpy(dd->owner, owner);
+dd->name = char_copy(dd->name, name);
+dd->owner = char_copy(dd->owner, owner);
 dd->age = age;
 }
 return (dd);
@@ -56,12 +56,12 @@ c++;
 return (x);
 }
 /**
- * _cpy - to copy a string
+ * char_copy - to copy a string
  * @dest: Destination value
  * @src: Source value
  * Return: the pointer to dest
  */
-char *_cpy(char *des, char *src)
+char *char_copy(char *des, char *src)
 {
 int x = 0;
 while (src[x] != '\0')
@@ -70,7 +70,6 @@ des[x] = src[x];
 x++;
 }
 des[x] = '\0';
-printf("%s", des);
 return (des);
 } 
 
