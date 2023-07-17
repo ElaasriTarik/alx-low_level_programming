fd453d8e4bb67a1efc88fb09e4a2c548d937eba9
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-17 19:44:21 +0100

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,12 @@ float age;
 char *owner;
 };
 
+/**
+ * dog_t - dog Typedef
+*/
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
