b16193900c3aeecaaefaf2eb4113b0944eee5e6d
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-17 16:07:00 +0100

    define 1024

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,3 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
@@ -9,6 +7,6 @@
  */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
