8e4aadbbc5309f24a137a137a95cf9b614f952ce
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-19 15:35:25 +0100

    func on each arr element

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -7,7 +7,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 int x = 0;
 if (array == NULL || action == NULL)
 return;
-for (; x < (int)size; x++)
+for (; x < (int) size; x++)
 action(array[x]);
 }
 /**
