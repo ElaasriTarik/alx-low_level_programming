80e9f9a52113aef25302383d674bb6d1b58d1dc8
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-19 17:57:05 +0100

    cmp

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,5 +11,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+int i = 0;
+if (size <= 0)
+return (-1);
 
+for (; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
 }
