202800733c601935184a83a4bebe15ad54f5a8e4
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-19 15:10:43 +0100

    pointer function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,13 +4,15 @@
 
 /**
  * print_name - prints a name
- * @name: name
- * @f: function to call
+ *@name: name
+ *@f: function to call
  * Return: Void
 */
 
 void print_name(char *name, void (*f)(char *))
 {
 if (f != NULL)
+{
 f(name);
+}
 }
