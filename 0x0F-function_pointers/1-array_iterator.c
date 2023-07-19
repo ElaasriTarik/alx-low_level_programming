438d1cba8e42cb2fa9f069b5f2eb37364b2c1c28
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-19 15:25:02 +0100

    func on each arr element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ *@size: size of the array
+ *@array: array
+ *@action: function to call
+ * Return: Void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int x = 0;
+for (; x < size; x++)
+{
+action(array[x]);
+}
+}
--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -2,46 +2,34 @@
 #include "function_pointers.h"
 
 
-void print_name(char *name, void (*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-(*f)(name);
+int x = 0;
+for (; x < size; x++)
+{
+action(array[x]);
 }
-
+}
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_elem - prints an integer
+ * @elem: the integer to print
  *
  * Return: Nothing.
  */
-void print_name_as_is(char *name)
+void print_elem(int elem)
 {
-    printf("Hello, my name is %s\n", name);
+    printf("%d\n", elem);
 }
 
 /**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
  *
  * Return: Nothing.
  */
-void print_name_uppercase(char *name)
+void print_elem_hex(int elem)
 {
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
+    printf("0x%x\n", elem);
 }
 
 /**
@@ -51,8 +39,9 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
+    int array[5] = {0, 98, 402, 1024, 4096};
+
+    array_iterator(array, 5, &print_elem);
+    array_iterator(array, 5, &print_elem_hex);
     return (0);
 }
