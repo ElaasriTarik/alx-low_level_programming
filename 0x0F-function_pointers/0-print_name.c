613f849294cf63e3523659c240f7219e6033de49
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-19 17:12:20 +0100

    removed libraries

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,13 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - prints a name
  *@name: name
  *@f: function to call
- * Return: Void
+ *Return: Void
 */
-
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
-{
+if (f)
 f(name);
-}
 }
