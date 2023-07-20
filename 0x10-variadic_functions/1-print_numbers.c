72e8d84f1e357bf535f7823b9ac184dc07a6c64b
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-20 21:21:55 +0100

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,36 +17,11 @@ va_start(args, n);
 for (; a < n; a++)
 {
 int x = va_arg(args, int);
-if (x < 0)
-{
-putchar('-');
-print_val(-x);
-}
-else if (x >= 0 && x < 10)
-{
-putchar(x + '0');
-}
-else
-print_val(x);
+printf("%d", x);
 if ((a < n - 1) && separator != NULL)
 {
-putchar(*separator);
-putchar(' ');
+printf("%s", separator);
 }
 }
-putchar('\n');
-}
-/**
- * print_val - prints values
- * @d: digit to be printed
- * Return: (Success)
- *
-*/
-void print_val(int d)
-{
-if(d != 0)
-{
-print_val(d / 10);
-putchar((d % 10) + '0');
-}
+printf("\n");
 }
