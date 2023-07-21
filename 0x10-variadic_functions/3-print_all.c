13419b0a5178205961fd80da43702d1c86a954fb
Abdessadek El Aasri <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-21 01:09:36 +0100

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,34 +19,33 @@ va_start(args, format);
 
 if (format)
 {
-    while (format[i])
-    {
-        switch (format[i])
-        {
-        case 'i':
-            printf("%s%d", bt, va_arg(args, int));
-            break;
-        case 'c':
-            printf("%s%c", bt, va_arg(args, int));
-            break;
-        case 'f':
-            printf("%s%f", bt, va_arg(args, double));
-            break;
-        case 's':
-            string = va_arg(args, char *);
-            if (string == NULL)
-            string = "(nil)";
+while (format[i])
+{
+switch (format[i])
+{
+case 'i':
+printf("%s%d", bt, va_arg(args, int));
+break;
+case 'c':
+printf("%s%c", bt, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", bt, va_arg(args, double));
+break;
+case 's':
+string = va_arg(args, char *);
+if (string == NULL)
+string = "(nil)";
 
-            printf("%s%s", bt, string);
-            break;
-            default:
-            i++;
-            continue;
-        }
-        bt = ", ";
-        i++;
-    }
-    
+printf("%s%s", bt, string);
+break;
+default:
+i++;
+continue;
+}
+bt = ", ";
+i++;
+}
 }
 printf("\n");
 }
