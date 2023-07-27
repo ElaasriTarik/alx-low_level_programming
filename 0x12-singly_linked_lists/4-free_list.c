aa2aea7c93024e5fcfd09cf597ee365ac42cfd75
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-27 23:56:37 +0100

    add_node_end

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of list
+ * @head: head of list
+ * @str: string
+ *
+ *Return: number of nodes.
+ */
+
+void free_list(list_t *head)
+{
+
+free(head);
+
+}
