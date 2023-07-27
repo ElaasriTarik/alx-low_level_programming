2c306965ef73eac60525e0e0ef7c43a073b31b2c
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-27 21:43:42 +0100

    list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - singly linked list
+ * @h: pointer to list
+ *
+ *Return: number of nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t x = 0;
+
+while (h)
+{
+h = h->next;
+x++;
+}
+return (x);
+}
