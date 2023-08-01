7c7f1194d50a55a580ec282a8f7f9055e534b92e
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-01 01:45:32 +0100

    Done!

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,14 @@
 
 void free_listint2(listint_t **head)
 {
+listint_t *tp;
 
-while (head != NULL)
+if (head == NULL)
+return;
+while (*head)
 {
+tp = (*head)->next;
 free(*head);
-head = NULL;
+*head = tp;
 }
 }
