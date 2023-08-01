1e488ef27bbde9d3edb65576e0d7f890e57f93a2
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-02 00:08:55 +0100

    Done!

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to list
+ * Return: number of elements
+ */
+size_t free_listint_safe(listint_t **h)
+{
+size_t x = 0;
+listint_t *temp;
+int dd;
+
+if (h != NULL || *h != NULL)
+return (0);
+
+while (*h != NULL)
+{
+dd = *h - (*h)->next;
+if (dd > 0)
+{
+temp = (*h)->next;
+*h = temp;
+x++;
+}
+else
+{
+*h = NULL;
+x++;
+break;
+}
+}
+*h = NULL;
+return (x);
+}
