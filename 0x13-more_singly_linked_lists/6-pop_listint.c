85f15ce92ee2e5804ad712433699dee90592e0dc
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-01 01:33:25 +0100

    Done!

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - adds a new node at the end of list
+ * @head: head of list
+ *Return: head value.
+ */
+
+int pop_listint(listint_t **head)
+{
+listint_t *temp2 = *head;
+listint_t *temp = *head;
+/** chech if list is empty*/
+if ((*head) == NULL)
+return (0);
+
+while (!temp->next)
+{
+temp2 = temp;
+temp = temp->next;
+}
+
+return (temp2->n);
+
+}
