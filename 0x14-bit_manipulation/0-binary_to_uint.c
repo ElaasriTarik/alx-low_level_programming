fb408700bc9657ead35996b812adf06b89205b1f
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-03 22:27:07 +0100

    Done!

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _pow(int d, int x);
+
 /**
  * binary_to_uint - converts a binary to integer
  *@b: binary code to convert
@@ -19,9 +21,30 @@ while (cc[x] != '\0')
 if (!(cc[x] >= 48 && cc[x] <= 57))
 return (0);
 bc = cc[x] - '0';
-rInt += bc * pow(2, len);
+rInt += bc * _pow(2, len);
 len--;
 x++;
 }
 return (rInt);
+}
+/**
+ * b_pow - powers a number
+ *@x: to the power of
+ *@d: number to be powered
+ * Return: powered result
+*/
+
+int _pow(int d, int x)
+{
+int sum = 2;
+if (x == 1)
+return (2);
+if (x == 0)
+return (1);
+
+for (; x >= 2; x--)
+{
+sum *= d;
+}
+return (sum);
 }
