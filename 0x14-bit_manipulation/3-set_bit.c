c98215db92c115140805e875bfb3742b6ecb10f3
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-04 01:02:28 +0100

    Done!

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit - gets bit at a certain index
+ *@index: index
+ @n: value
+ * Return: (Success)
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 32)
+return (-1);
+
+return ((n >> index) & 1);
+}
