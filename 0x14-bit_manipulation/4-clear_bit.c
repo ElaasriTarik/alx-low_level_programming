5784672219f795034cff86721a31f417ccf28ee0
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-04 01:10:15 +0100

    Done!

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,8 +8,11 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+unsigned int curr;
 if (index > 64)
 return (-1);
 
-return (*n |= ~(1 << index));
+curr = ~(1 << index);
+*n &= curr;
+return (1);
 }
