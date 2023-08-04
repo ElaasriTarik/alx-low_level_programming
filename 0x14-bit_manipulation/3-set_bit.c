6a5a7cd53578f3c950b4303f1cd68063d2971a6a
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-04 01:05:23 +0100

    Done!

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,13 +3,13 @@
 /**
  * set_bit - gets bit at a certain index
  *@index: index
- @n: value
+ *@n: value
  * Return: (Success)
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index > 32)
+if (index > 64)
 return (-1);
 
-return ((n >> index) & 1);
+return (*n |= (1 << index));
 }
