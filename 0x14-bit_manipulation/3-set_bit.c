c5d840a7435da70420358428cc9c1f425ec6e4a0
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-04 01:10:49 +0100

    Done!

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,8 +8,10 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned int curr;
 if (index > 64)
 return (-1);
-
-return (*n |= (1 << index));
+curr = (1 << index);
+*n |= curr;
+return (1);
 }
