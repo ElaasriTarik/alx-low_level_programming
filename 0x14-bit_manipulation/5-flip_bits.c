3e9106154b3ea3a1299e153dc9dc8c1d593ebb6f
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-04 01:19:22 +0100

    Done!

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,12 +10,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int compare = n ^ m;
 unsigned long int x = 0;
-unsigned int num = 0;
-
-while (compare) {
+unsigned int count = 0;
+while (compare)
+{
 x = compare & 1;
-num += x;
+count += x;
 compare >>= 1;
 }
-return num;
+return count;
 }
