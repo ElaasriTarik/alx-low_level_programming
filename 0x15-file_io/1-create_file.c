978d039e4db8bbf5c5154f549b58ee212e946016
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-07 21:56:15 +0100

    Done!

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,30 +8,24 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int fd, count;
-ssize_t wBytes;
+int fd, count, wBytes = 0;
 if (filename == NULL)
 return (-1);
 
-if (text_content == NULL)
-fd = open(filename, O_RDWR | O_CREAT | S_IRUSR | S_IWUSR);
-
-fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
 
-while (text_content != NULL)
-count++;
-
+if (text_content)
+{
+while (text_content[count])
 count++;
 wBytes = write(fd, text_content, count);
-
-if (wBytes == -1)
+if (wBytes != count)
 {
-close(fd);
 return (-1);
 }
-
+}
 
 close(fd);
 return (1);
