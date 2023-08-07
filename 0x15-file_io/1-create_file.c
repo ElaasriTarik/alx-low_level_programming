972d8dd7855ddccc21cfc8939be13a93e36c98b3
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-07 22:01:55 +0100

    Done!

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,8 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd, count, wBytes = 0;
-if (filename == NULL)
+
+if (!filename)
 return (-1);
 
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
