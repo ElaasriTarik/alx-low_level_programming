bc21b5b17f8f5c8afb4629c930fd1351147d8f01
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-07 21:48:39 +0100

    Done!

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,18 @@ if (filename == NULL)
 return (-1);
 
 if (text_content == NULL)
-{
 fd = open(filename, O_RDWR | O_CREAT | S_IRUSR | S_IWUSR);
-}
 
 fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
+
 while (text_content != NULL)
 count++;
 
+count++;
 wBytes = write(fd, text_content, count);
+
 if (wBytes == -1)
 {
 close(fd);
