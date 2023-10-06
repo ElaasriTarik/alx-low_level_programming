9206f0383dfc1559540aff14f3b50c56c4577b48
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-06 17:12:47 +0100

    hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - creates a hash table
  * @size: size of the hash table
  * Return: the hash table (Success)
-*/
+ */
 
 
 hash_table_t *hash_table_create(unsigned long int size)
