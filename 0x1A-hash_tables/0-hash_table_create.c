39006c4afb0cc50b05d870e2bc7fa00c05b9cf94
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-06 16:28:46 +0100

    hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
-table->size = size;
+  hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+  unsigned long int x = 0;
+  if (table == NULL)
+    return (NULL);
+  table->size = size;
 
-table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+  table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 
-if (table == NULL || table->array == NULL)
-return (NULL);
+  if (table == NULL || table->array == NULL)
+    return (NULL);
+  for (; x < size; x++)
+    {
+      table->array[x] = NULL;
+    }
 
-return (table);
+  return (table);
 }
