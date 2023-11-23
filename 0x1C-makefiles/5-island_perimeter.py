e057d52ddfe1235311f9fe34af678dc2393f66f5
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-23 11:36:45 +0100

    updated

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,11 +12,17 @@ def island_perimeter(grid):
         ix = 0
         for ix in range(len(grid[x])):
             if grid[x][ix] == 1:
-                prev = x - 1
-                nxt = x + 1
-                if grid[prev][ix] != 1:
+                if x - 1 >= 0:
+                    prev = x - 1
+                    if grid[prev][ix] != 1:
+                        total += 1
+                else:
                     total += 1
-                if grid[nxt][ix] != 1:
+                if x + 1 < len(grid):
+                    nxt = x + 1
+                    if grid[nxt][ix] != 1:
+                        total += 1
+                else:
                     total += 1
                 if grid[x][x - 1] != 1:
                     total += 1
--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -21,3 +21,14 @@ if __name__ == "__main__":
         [0, 0, 0, 0],
     ]
     print(island_perimeter(grid5))  # Output: 8
+
+    grid3 = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0]
+    ]
+    print(island_perimeter(grid3)) #output: 12
+
+    grid4 = [[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]]
+    print(island_perimeter(grid4)) #output: 16
