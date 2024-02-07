700dd85d3e435c36ebb0d06f0e8c5bec5a1a7667
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-08 00:57:50 +0100

    interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * interpolation_search - function to search for values linearily
+ * @array: array of the value
+ * @size: size of the array
+ * @value: value to searc for
+ * Return: result index or -1 if not found
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t high = size - 1;
+	size_t low = 0;
+	size_t pos = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (value >= array[low] && value <= array[high] && low <= high)
+	{
+		pos = (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos += low;
+		if (pos >= size)
+			printf("Value checked array[%ld] is out of range\n", pos);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+		{
+			low = pos + 1;
+		}
+		else
+		{
+			high = pos - 1;
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int search(size_t l, size_t r, int *array, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
