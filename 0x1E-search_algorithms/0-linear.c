d04415da76bfd7e1ce55a9c328ce9eacac57ee73
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-07 13:00:43 +0100

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,19 +9,19 @@
  * @size: size of the array
  * @value: value to searc for
  * Return: result index or -1 if not found
-*/
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
-		size_t x = 0;
+	size_t x = 0;
 
-		for (; x < size; x++)
+	for (; x < size; x++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+		if (array[x] == value)
 		{
-				printf("Value checked array[%ld] = [%d]\n", x, array[x]);
-				if (array[x] == value)
-				  {
-					return (x);
-				  }
+			return (x);
 		}
-		return (-1);
+	}
+	return (-1);
 }
